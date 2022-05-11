18053c1379d767c1f5756aa10a5d8b250ab97946
eliezerrb <eliezerrezende@outlook.com>
2022-05-10 22:58:37 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,34 @@
 import Pagination from "components/Pagination";
+import MovieCard from "components/MovieCard";
 
-function Listing(){
+function Listing() {
 
-    return(
-        <Pagination />
+    return (
+        <>
+
+            <div className="container">
+                <Pagination />
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+
+        </>
+
     );
 
 }
